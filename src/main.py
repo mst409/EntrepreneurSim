7cc0ef7587bfe883a44d204aecdf5119c6cb9f41
Theacebutler <Theacebutler@gmail.com>
2025-10-16 22:59:11 -0400

    added players and buisness routers

--- src/main.py
+++ src/main.py
@@ -1,9 +1,13 @@
 from fastapi import FastAPI
 from src.database import create_tables
 from src.auth import routes as auth
+from src.players import routes as players
+from src.businesses import routes as businesses
 
 app = FastAPI()
 app.include_router(auth.router)
+app.include_router(players.router)
+app.include_router(businesses.router)
 
 
 # Create database tables on startup
