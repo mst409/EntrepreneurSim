c6a9dc423e86b5a89e47b98ae33ea494fc3ba8a5
Theacebutler <Theacebutler@gmail.com>
2025-10-17 17:05:22 -0400

    changed table name to businesses

--- src/businesses/models.py
+++ src/businesses/models.py
@@ -4,9 +4,10 @@ from sqlalchemy import Column, Integer, String, ForeignKey, Uuid
 from src.database import Base
 
 class Business(Base):
-    __tablename__ = "business"
+    __tablename__ = "businesses"
 
     id = Column(Uuid(as_uuid=True), primary_key=True, index=True, default=uuid.uuid4)
     # TODO #3 chenge to UUID4 after the user id is chenged
     owner = Column(Integer, ForeignKey("players.id", ondelete="CASCADE"))
     business_name = Column(String(20), nullable=False, unique=True)
+    # bank_account = Column(Uuid, ForeignKey("bank_accounts.id"))
