eb66ee0f50c46ba503b3a81c95c1c71144418edf
Theacebutler <Theacebutler@gmail.com>
2025-10-22 23:33:01 -0400

    created testing for createing a user

--- tests/auth_tsest.py
+++ tests/auth_tsest.py
@@ -2,13 +2,33 @@ from fastapi.testclient import TestClient
 
 
 def test_create_user(client: TestClient):
-    res = client.post("/auth/signup", json={
-  "user_name": "testuser",
-  "email": "user@example.com",
-  "password": "testpassword"
-})
-    assert res.status_code == 200
-
-def test_get_new_player(client: TestClient):
-    response = client.get("/players/name/testuser")
+    # test creating a new user
+    response = client.post("/auth/signup", json={
+        "user_name": "testuser",
+        "email": "user@example.com",
+        "password": "testpassword"
+    })
     assert response.status_code == 200
+    assert isinstance(response.json(), dict)
+
+def create_email_already_registerd(client: TestClient):
+    '''test creating a user with a diffrent username and email 
+    that is already taken'''
+
+    client.post("/auth/signup", json={
+        "user_name": "testuser",
+        "email": "user@example.com",
+        "password": "testpassword"
+    })
+
+    response = client.post("/auth/signup", json={
+        "user_name": "testuser2",
+        "email": "user@example.com",
+        "password": "testpassword"
+    })
+    assert response.status_code == 400
+    assert response.json() == {"detail": "Email already registered"}
+
+    # TODO test the new player that is auto generated
+    # TODO test the players bank account
+    
