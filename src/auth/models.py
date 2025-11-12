37c2fe5886a34eff03f3d828cbc82b06775ac77c
theacebutler <theacebutler@gmail.com>
2025-11-11 19:33:59 -0500

    added relationship from users table to players table

--- src/auth/models.py
+++ src/auth/models.py
@@ -1,7 +1,7 @@
 # this file contains the database models for the authentication system
 import datetime as dt
 import uuid
-from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
 from sqlalchemy.types import Uuid
 from sqlalchemy.orm import relationship
 from src.database import Base
@@ -10,14 +10,13 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(Uuid(as_uuid=True), primary_key=True, index=True, default=uuid.uuid4)
-    user_name = Column(String, unique=True, index=True)
     email = Column(String, unique=True, index=True)
     hashed_password = Column(String)
     created_at = Column(DateTime, default=dt.datetime.now(dt.timezone.utc))
     bio = Column(String(255), null=True)
-    # add an option for a profile pic
+    #TODO add an option for a profile pic
     profile = Column(String(255), null=True)
     
+    player_id = Column(Uuid, ForeignKey("players.id"), nullable=True)
     
-    
-    player = relationship("Player", back_populates="user_info", cascade="all, delete-orphan")
+    player = relationship("Player", back_populates="user_info", cascade="all, delete-orphan", foreign_keys=[player_id])
