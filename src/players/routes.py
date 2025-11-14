65f0cda270007b2cbf6098e30598fd6449a0d458
theacebutler <theacebutler@gmail.com>
2025-11-13 21:03:54 -0500

    fixed response model in returning player my name

--- src/players/routes.py
+++ src/players/routes.py
@@ -6,9 +6,10 @@ from sqlalchemy.orm import Session
 
 from src.database import get_db
 from src.auth.models import User
+from src.auth.schemas import UserResponse
 from src.players.models import Player
 from src.players.schemas import PlayerCreate, PlayerResponse
-from src.players.servies import auto_create_player
+from src.players.services import auto_create_player
 
 
 router = APIRouter(prefix="/players", tags=["players"])
@@ -17,21 +18,10 @@ router = APIRouter(prefix="/players", tags=["players"])
 
 @router.get("/name/{name}", response_model=PlayerResponse)
 async def get_player_by_name(name: str, db: Session = Depends(get_db)):
-
-    # get the user id for the name in the request
-    user = db.query(User).filter(User.user_name == name).first()
-    if not user: 
-        raise HTTPException(status_code=404, 
-                            detail=f"player with name '{name}' not found")
-    
-    # get the player obj with the id
-    player = db.query(Player).filter(Player.user_id == user.id).first()
-    if not player:
-        raise HTTPException(status_code=404,
-                            detail=f'player with name "{name}" not found')
-    # TODO #2 find how to get all the data in one query with a join
-
+    player = db.query(Player).join(Player.user_info).filter(User.name == name).first()
     return player
+
+
 
 
 @router.delete("/delete/{name}", response_model=dict[str, str])
