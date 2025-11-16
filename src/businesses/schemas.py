040cc0a24ff03adbb587045b1847fb5f68f842d7
theacebutler <theacebutler@gmail.com>
2025-11-16 10:32:39 -0500

    added TODO to businesses models

--- src/businesses/schemas.py
+++ src/businesses/schemas.py
@@ -1,8 +1,6 @@
-import datetime
 from enum import Enum
-from typing import Any
 from pydantic import BaseModel, Field, UUID4
-import uuid
+
 
 class PayRoll(str, Enum):
     first = '00',
@@ -18,7 +16,6 @@ class BusinessCreate(BaseBusiness):
 class BusinessResponse(BaseBusiness):
     # TODO add the owner name to the response model
     id: UUID4
-
 
     class Config:
         from_attributes = True
