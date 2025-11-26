83d1330d4118787bc0f4edef589ba3f204f893ee
theacebutler <theacebutler@gmail.com>
2025-11-25 22:03:21 -0500

    minor changes

--- src/businesses/schemas.py
+++ src/businesses/schemas.py
@@ -1,21 +1,35 @@
 from enum import Enum
 from pydantic import BaseModel, Field, UUID4
 
+from src.players.models import Owners
+
 
 class PayRoll(str, Enum):
-    first = '00',
-    fifteenth = '15'
+    first = ("00",)
+    fifteenth = "15"
+
+
+class Owner(BaseModel):
+    # business_id: BaseBusiness
+    # player_id: UUID4
+
+    class Config:
+        from_attributes = True
+
 
 class BaseBusiness(BaseModel):
-    business_name:str = Field(max_length=20)
+    business_name: str = Field(max_length=20)
     payroll: PayRoll
+
 
 class BusinessCreate(BaseBusiness):
     owner_name: str
 
+
 class BusinessResponse(BaseBusiness):
     # TODO add the owner name to the response model
     id: UUID4
+    owner: Owner
 
     class Config:
         from_attributes = True
