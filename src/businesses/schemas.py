2516b200df029336fa5396d6a7f0f8f5de9d8071
theacebutler <theacebutler@gmail.com>
2025-11-26 23:30:34 -0500

    schema for player with business info works

--- src/businesses/schemas.py
+++ src/businesses/schemas.py
@@ -7,15 +7,6 @@ class PayRoll(str, Enum):
     first = ("00",)
     fifteenth = "15"
 
-
-class Owner(BaseModel):
-    # business_id: BaseBusiness
-    # player_id: UUID4
-
-    class Config:
-        from_attributes = True
-
-
 class BaseBusiness(BaseModel):
     business_name: str = Field(max_length=20)
     payroll: PayRoll
@@ -28,7 +19,6 @@ class BusinessCreate(BaseBusiness):
 class BusinessResponse(BaseBusiness):
     # TODO add the owner name to the response model
     id: UUID4
-    owner: Owner
 
     class Config:
         from_attributes = True
--- src/players/schemas.py
+++ src/players/schemas.py
@@ -1,9 +1,9 @@
 import datetime
+from typing import Any
 from pydantic import BaseModel, Field, UUID4, EmailStr
 
 from src.auth.schemas import UserBase
 from src.banking.schemas import BankAccountResponse 
-# from src.businesses.schemas import BusinessResponse
 
 class BasePlayer(BaseModel):
     pass
@@ -12,22 +12,20 @@ class PlayerCreate(BasePlayer):
     user_name: str | None
     user_email: EmailStr | None
 
-# class PlayerBusinessResponse(BasePlayer):
-#     id: UUID4
-#     user_name: str | None
-#     created_at: datetime.datetime
-
-#     class Config: 
-#         from_attributes = True
-
+class BusinessForPlayer(BaseModel):
+    business_name: str
+    payroll: str | Any
 
 class PlayerResponse(BasePlayer):
     id: UUID4
     user_info: UserBase 
     bank_account: BankAccountResponse   
-    # business_info: BusinessResponse
+    business: list[ BusinessForPlayer ]
     class Config: 
         from_attributes = True
+
+
+    
 
 class EmployeeBase(BaseModel):
     roll: str
